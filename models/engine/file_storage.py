11f95599deea8cbf66f36d90498155b6b157f2fd
Ayobami <ayobamidele006@gmail.com>
2023-05-28 21:22:27 +0100

    fix file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,6 +11,7 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+import models
 
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
@@ -83,9 +84,6 @@ class FileStorage:
             matchstring = cls + '.' + id
             if k == matchstring:
                 return v
-
-
-
 
     def count(self, cls=None):
         """count the number of objects in storage"""
