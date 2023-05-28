b5fa90034b9227d1f3fc5ae037436551a545ef49
Ayobami <ayobamidele006@gmail.com>
2023-05-28 12:17:02 +0100

    be simple in if name block

--- api/v1/app.py
+++ api/v1/app.py
@@ -21,16 +21,7 @@ def teardown_db(exception):
     """ Closes the storage """
     storage.close()
 
-if __name__ == '__main__':
-    if getenv("HBNB_API_HOST") is None:
-        HBNB_API_HOST = '0.0.0.0'
-    else:
-        HBNB_API_HOST = getenv("HBNB_API_HOST")
-    if getenv("HBNB_API_PORT") is None:
-        HBNB_API_PORT = 5000
-    else:
-        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
-    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
+
 if __name__ == "__main__":
     app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
             port=int(getenv('HBNB_API_PORT', '5000')),
