53a300b53ffc6359c2ff2cacd094dc78f735501f
MamaKbubu <111245031+MamaKbubu@users.noreply.github.com>
2023-05-28 12:03:35 +0200

    Update app.py
    I just wanna see which error comes up

--- api/v1/app.py
+++ api/v1/app.py
@@ -20,9 +20,20 @@ app.register_blueprint(app_views)
 def teardown_db(exception):
     """ Closes the storage """
     storage.close()
+ 
+@app.errorhandler(404)
+def not_found(error):
+    """ handles the 404 error and gives a json formatted response"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-if __name__ == "__main__":
-    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(getenv('HBNB_API_PORT', '5000')),
-            threaded=True)
+if __name__ == '__main__':
+    if getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
+    else:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
