df64bb564ddeb36aaa8c6e4cc947d4c5ad343eeb
Ayobami <ayobamidele006@gmail.com>
2023-05-30 09:30:56 +0100

    add strict_slashe to route
    Co-authored-by: MamaKbubu <Maboelatk@gmail.com>

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,6 +8,7 @@ from api.v1.views import app_views
 from models import storage
 from models.city import City
 from models.place import Place
+from models.user import User
 
 
 def get_object_by_id(model_cls, object_id):
@@ -18,8 +19,8 @@ def get_object_by_id(model_cls, object_id):
     return obj
 
 
-@app_views.route('/cities/<city_id>/places', methods=['GET'])
-@app_views.route('/cities/<city_id>/places/', methods=['GET'])
+@app_views.route('/cities/<city_id>/places', methods=['GET'],
+                 strict_slashes=False)
 def list_places_of_city(city_id):
     """Retrieves a list of all Place objects in a city"""
     city = get_object_by_id(City, city_id)
@@ -27,14 +28,15 @@ def list_places_of_city(city_id):
     return jsonify(list_places)
 
 
-@app_views.route('/places/<place_id>', methods=['GET'])
+@app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
 def get_place(place_id):
     """Retrieves a Place object"""
     place = get_object_by_id(Place, place_id)
     return jsonify(place.to_dict())
 
 
-@app_views.route('/places/<place_id>', methods=['DELETE'])
+@app_views.route('/places/<place_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_place(place_id):
     """Deletes a Place object"""
     place = get_object_by_id(Place, place_id)
@@ -43,7 +45,8 @@ def delete_place(place_id):
     return jsonify({}), 200
 
 
-@app_views.route('/cities/<city_id>/places', methods=['POST'])
+@app_views.route('/cities/<city_id>/places', methods=['POST'],
+                 strict_slashes=False)
 def create_place(city_id):
     """Creates a Place"""
     data = request.get_json()
@@ -53,10 +56,10 @@ def create_place(city_id):
         abort(400, 'Missing user_id')
     if 'name' not in data:
         abort(400, 'Missing name')
-    city = get_object_by_id("City", city_id)
+    city = get_object_by_id(City, city_id)
     if not city:
         abort(404)
-    user = get_object_by_id("User", data['user_id'])
+    user = get_object_by_id(User, data['user_id'])
     if not user:
         abort(404)
     new_place = Place(name=data['name'],
@@ -67,10 +70,10 @@ def create_place(city_id):
     return jsonify(new_place.to_dict()), 201
 
 
-@app_views.route('/places/<place_id>', methods=['PUT'])
+@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
 def updates_place(place_id):
     """Updates a Place object"""
-    place = get_object_by_id("Place", place_id)
+    place = get_object_by_id(Place, place_id)
     data = request.get_json()
     if not data:
         abort(400, 'Not a JSON')
